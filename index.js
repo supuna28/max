f3861db1cf26dfd87ccd2f91a169293e693aaa82
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-10-22 10:39:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ global.antidelete = false
 
 conn.version = [2, 2119, 6]
 conn.logger.level = "warn"
-
+conn.browserDescription = ['R I X L E ™','EDGE','94.0.992.50']
 if (fs.existsSync('./session.json')) conn.loadAuthInfo('./session.json')
    conn.on('qr', qr => {
    console.log(`PLEASE SCAN QR`)
